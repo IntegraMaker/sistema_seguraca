6f6b99947fceee2a4de900c82df13870285b4115
Diego <dev.diegotomaz@gmail.com>
2025-11-03 09:12:03 -0300

    visualização de validade QRCode

--- blueprints/pessoa_bp.py
+++ blueprints/pessoa_bp.py
@@ -8,5 +8,14 @@ def carteiraPessoa():
     if "id" in session:
         cpf = request.values.get("cpf")
         registro = buscaPessoa(cpf)
-        return render_template("pessoa/carteira.html", dadosPessoa=registro)
+        if registro.qrcode:
+            caminho = (str(registro.qrcode).split("\\"))
+            qrCode = caminho[1].split("_")
+            print(qrCode[0])
+            estadoQrcode = "Válido"
+            if (int(datetime.now().strftime("%Y%m%d")) - int(qrCode[0])) > 30000:
+                estadoQrcode = "Inválido"
+            print(estadoQrcode)
+
+        return render_template("pessoa/carteira.html", dadosPessoa=registro, estadoQrcode=estadoQrcode)
     return render_template("login.html")
